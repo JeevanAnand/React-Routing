595627587806539fdbbaa15aad6d461ae3fe5d4d
JeevanAnand <jeev46@gmail.com>
2021-04-23 15:33:37 +0530

    Programatic Navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    //this.props.history.push('/products'); //when back button is clicked on browser we will see previous page
+    this.props.history.replace('/products'); //when back button is clicked on browser we wont be able to see previous page
   };
 
   render() {
