b6dbd89d8988a8c720d8edc16f8034602073eaf0
JeevanAnand <jeev46@gmail.com>
2021-04-23 13:17:33 +0530

    adding routing parameters

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
+//Register our Routs , meaning we need to tell react component should be rendered based on a given URL
 
+//while using Switch component , you should order your routes from most specific ones to most generic ones
+
+// Route component is a wrapper around the component that we pass ,If the path matches it will render the component
+//and it will automatically inject the  props (history,location,match) into the component
 class App extends Component {
   render() {
     return (
@@ -16,7 +21,10 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products" component={Products} />
+            {/* <Route path="/products" component={Products} /> In this case component holds default props */}
+            {/* <Route path="/products" render={()=><Products sort="newest"/>}/> In this case we are overwriting default props  */}
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route path="/products" render={(props) => <Products sort="newest"{...props} />} />   {/* In this case we are including both default and component specific props  */}
             <Route path="/posts" component={Posts} />
             <Route path="/admin" component={Dashboard} />
             <Route path="/" exact component={Home} />
